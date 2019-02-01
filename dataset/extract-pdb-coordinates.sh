2ba082e8c8acf1f64f639b6ec3132893389b55f8
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-02-01 23:53:04 +0100

    seems to be good

--- dataset/extract-pdb-coordinates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `cat pdbid-lowercase.txt`;
-do
-#	echo $i
-	[ ! -e $i.pdb ] && `wget https://files.rcsb.org/download/$i.pdb` #--header "Referer: https://files.rcsb.org"
-	sleep .1
-done
--- parse_dssp_output.sh
+++ parse_dssp_output.sh
@@ -3,19 +3,24 @@
 mkdir -p ./dataset/
 mkdir -p ./dataset/pdb_files
 #  Get PDB id, remove header and remove chains identifier (last char of PDB ID)
-awk '{ print $1 }' ./dataset/cullpdb_pc20_res1.6_R0.25_d190117_chains3429 | tail -n +2 | cut -c1-4  > ./dataset/list_of_id.txt
+list_of_id=`awk '{ print $1 }' ./dataset/cullpdb_pc20_res1.6_R0.25_d190117_chains3429 | tail -n +2 | cut -c1-4`
 # Loop on every ID and wget it
-for i in `cat ./dataset/list_of_id.txt`; do
-	[ ! -e $i.pdb ] && `wget https://files.rcsb.org/download/$i.pdb -P ./dataset/pdb_files`
-	sleep 0.1
-done
-# Compress files inot one archive
-zip ./dataset/all_pdb_files.zip ./dataset/pdb_files/*.pdb
+# for i in $list_of_id; do
+# 	[ ! -e ./dataset/pdb_files/$i.pdb ] &&  `wget https://files.rcsb.org/download/$i.pdb -P ./dataset/pdb_files/`
+# 	sleep 0.1
+# done
+# Compress files into one archive
+# zip ./dataset/all_pdb_files.zip ./dataset/pdb_files/*.pdb
+# Delete temporary files
 rm ./dataset/pdb_files/*.pdb
-rm ./dataset/list_of_id.txt
-
 
 mkdir -p ./dssp_output/raw_dssp
+
+for i in $list_of_id; do
+	[ ! -e ./dataset/pdb_files/$i.pdb ] && unzip -j "./dataset/all_pdb_files.zip" "dataset/pdb_files/$i.pdb" -d "./dataset/pdb_files/"
+	./bin_dssp/dssp-2.0.4-linux-amd64 -i ./dataset/pdb_files/$i.pdb -o ./dssp_output/raw_dssp/$i.dssp
+	rm ./dataset/pdb_files/$i.pdb
+done
 
 
 mkdir -p ./dssp_output/sequence
@@ -25,19 +30,18 @@ accessibility="./dssp_output/solvent_accessibility/"
 mkdir -p ./dssp_output/angles
 angles="./dssp_output/angles/"
 
-#Save Amino acid sequence from each DSSP result in a new file
-for i in `ls ./dssp_output/raw_dssp/*.dssp`; do #la colone 14 est vire
+for i in `ls ./dssp_output/raw_dssp/*.dssp`; do
     name=$(basename $i)
     name=$(echo "$name" | cut -f 1 -d '.')
-    echo 'Processing to save Amino acid sequence of: ' $name
+    echo 'Parsing DSSP results of: ' $name
+	#Save Amino acid sequence from each DSSP result in a new file
     tail -n +29 $i | awk -F "" '{print $14}' | tr -d '\n' > "$sequence$name.seq"
 	#Save solvent accessibility informations (column 36,37 and 38 in DSSP result files) in a new file
-	echo 'Processing to save solvent accessibility informations of: ' $name
 	tail -n +29 $i | awk -F "" '{print $36 $37 $38}' | tr '\n' ',' | sed 's/\s//g' > "$accessibility$name.acc"
 	# Save PHI and PSI values in a new file
 	phi=$(tail -n +29 $i | awk -F "" '{print substr($0,104,6)}' | tr '\n' ',' | sed 's/\s//g')
 	psi=$(tail -n +29 $i | awk -F "" '{print substr($0,110,6)}' | tr '\n' ',' | sed 's/\s//g')
 	echo $phi > "$angles$name.ang"
-	echo $psi > "$angles$name.ang"
+	echo $psi >> "$angles$name.ang"
 	rm $i
 done
