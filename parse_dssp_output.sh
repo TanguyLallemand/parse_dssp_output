73d5d7ce84014cd02a8c12767849a269526754ab
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-02-02 11:00:26 +0100

    final cut

--- parse_dssp_output.sh
+++ parse_dssp_output.sh
@@ -13,16 +13,16 @@ list_of_id=`awk '{ print $1 }' ./dataset/cullpdb_pc20_res1.6_R0.25_d190117_chain
 # zip ./dataset/all_pdb_files.zip ./dataset/pdb_files/*.pdb
 # Delete temporary files
 rm ./dataset/pdb_files/*.pdb
-
+# Create directory to store DSSP output
 mkdir -p ./dssp_output/raw_dssp
-
+echo 'COmpute DSSP'
 for i in $list_of_id; do
 	[ ! -e ./dataset/pdb_files/$i.pdb ] && unzip -j "./dataset/all_pdb_files.zip" "dataset/pdb_files/$i.pdb" -d "./dataset/pdb_files/"
-	./bin_dssp/dssp-2.0.4-linux-amd64 -i ./dataset/pdb_files/$i.pdb -o ./dssp_output/raw_dssp/$i.dssp
-	rm ./dataset/pdb_files/$i.pdb
+	[ ! -e ./dssp_output/raw_dssp/$i.dssp ] && ./bin_dssp/dssp-2.0.4-linux-amd64 -i ./dataset/pdb_files/$i.pdb -o ./dssp_output/raw_dssp/$i.dssp
+	rm -f ./dataset/pdb_files/$i.pdb
 done
 
-
+# Create directories to store DSSP parsed results
 mkdir -p ./dssp_output/sequence
 sequence="./dssp_output/sequence/"
 mkdir -p ./dssp_output/solvent_accessibility
@@ -31,6 +31,7 @@ mkdir -p ./dssp_output/angles
 angles="./dssp_output/angles/"
 
 for i in `ls ./dssp_output/raw_dssp/*.dssp`; do
+	# Get name of PDB
     name=$(basename $i)
     name=$(echo "$name" | cut -f 1 -d '.')
     echo 'Parsing DSSP results of: ' $name
@@ -41,6 +42,7 @@ for i in `ls ./dssp_output/raw_dssp/*.dssp`; do
 	# Save PHI and PSI values in a new file
 	phi=$(tail -n +29 $i | awk -F "" '{print substr($0,104,6)}' | tr '\n' ',' | sed 's/\s//g')
 	psi=$(tail -n +29 $i | awk -F "" '{print substr($0,110,6)}' | tr '\n' ',' | sed 's/\s//g')
+	# store those informations, phi in first line, psi in seconde line
 	echo $phi > "$angles$name.ang"
 	echo $psi >> "$angles$name.ang"
 	rm $i
